296315fdd99fb5fe2e552d218cabd4336147a154
moldo <net.vrabie@gmail.com>
2025-05-12 22:05:35 +0200

    added the open-api dependency and swagger ui, however it does not work properly

--- src/main/java/com/execodex/sparrowair2/routes/AirportRoutes.java
+++ src/main/java/com/execodex/sparrowair2/routes/AirportRoutes.java
@@ -22,19 +22,18 @@ public class AirportRoutes {
     @Bean
     public RouterFunction<ServerResponse> airportRoutesFunction() {
         return RouterFunctions.route()
-                .path("/airports", builder -> builder
+//                .path("/airports", builder -> builder
                         // GET /airports - Get all airports
-                        .GET("", accept(MediaType.APPLICATION_JSON), airportHandler::getAllAirports)
-                        .GET("", accept(MediaType.APPLICATION_XML), airportHandler::getAllAirports)
+                        .GET("airports", accept(MediaType.APPLICATION_JSON), airportHandler::getAllAirports)
                         // GET /airports/{icaoCode} - Get airport by ICAO code
-                        .GET("/{icaoCode}", accept(MediaType.APPLICATION_JSON), airportHandler::getAirportByIcaoCode)
+                        .GET("airports/{icaoCode}", accept(MediaType.APPLICATION_JSON), airportHandler::getAirportByIcaoCode)
                         // POST /airports - Create a new airport
-                        .POST("", accept(MediaType.APPLICATION_JSON), airportHandler::createAirport)
+                        .POST("airports", accept(MediaType.APPLICATION_JSON), airportHandler::createAirport)
                         // PUT /airports/{icaoCode} - Update an existing airport
-                        .PUT("/{icaoCode}", accept(MediaType.APPLICATION_JSON), airportHandler::updateAirport)
+                        .PUT("airports/{icaoCode}", accept(MediaType.APPLICATION_JSON), airportHandler::updateAirport)
                         // DELETE /airports/{icaoCode} - Delete an airport
-                        .DELETE("/{icaoCode}", airportHandler::deleteAirport)
-                )
+                        .DELETE("airports/{icaoCode}", airportHandler::deleteAirport)
+//                )
                 .build();
     }
 }
